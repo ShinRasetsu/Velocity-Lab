0646c544499354460b615dff49151a58630b8174
ShinRasetsu <strikefreedomnine@gmail.com>
2026-08-28 23:15:26 +0800

    Velocity-Lab PWA Update: ~0,4datetime:~4,2datetime:~6,2 [~8,2datetime:~10,2datetime:~12,2]

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'velocity-lab-v1.4.2';
+const CACHE_VERSION = 'velocity-lab-v1.4.3';
 
 const APP_SHELL_CACHE = `shell-${CACHE_VERSION}`;
 const RUNTIME_CACHE   = `runtime-${CACHE_VERSION}`;
